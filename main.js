2ff1a20a513e6e516801d3cf4a616ab7d1d2c11d
J-Groesbeck <144719158+J-Groesbeck@users.noreply.github.com>
2024-12-06 14:21:02 +0000

    Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'This product is amazing!'
+            description: 'This product is amazing!',
+            image: './assets/images/socks_green.jpg',
+            url: 'index.html'
         }
     },
 })
