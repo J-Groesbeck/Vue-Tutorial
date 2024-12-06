7f08fea9feda054f357b0b2397b6d4f8315c74d5
J-Groesbeck <144719158+J-Groesbeck@users.noreply.github.com>
2024-12-06 14:12:42 +0000

    Creating the Vue App

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'This product is amazing!'
+        }
+    },
+})
